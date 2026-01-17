74f96762d9e3fcc354603b607c271c4fbfed8acc
m3au <m3au@pm.me>
2026-01-17 22:23:13 +0100

    feat: add Acapella and Bima to team page

--- src/data/team.ts
+++ src/data/team.ts
@@ -15,6 +15,18 @@ export const teamMembers: TeamMember[] = [
 		image: "/team/m3aus.jpg",
 	},
 	{
+		name: "Acapella",
+		role: "Head of Marketing",
+		twitter: "https://x.com/justmeowens_",
+		image: "/team/acapella.jpg",
+	},
+	{
+		name: "Bima",
+		role: "Head of Brand Strategy",
+		twitter: "https://x.com/Bimadesage",
+		image: "/team/bima.jpg",
+	},
+	{
 		name: "Franklin",
 		role: "Project Ambassador",
 		twitter: "https://twitter.com/big_franklin01",
